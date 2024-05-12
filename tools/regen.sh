6822463d4c2821cac8860c34e19c31ffb7be4bab
Santoso Wijaya <code@swijaya.me>
2024-05-12 14:19:56 -0700

    Fix `tools/regen.sh` script

--- tools/regen.sh
+++ tools/regen.sh
@@ -4,32 +4,33 @@
 
 # TODO: This only works in my own workspace, as base16cs-cli isn't deployed yet.
 BASE16CS=../base16cs
+RENDER="cargo run --features=cli --bin render"
 CWD=$(PWD)
 
 pushd ${BASE16CS}
 
-cargo run -p base16cs-cli -- \
+${RENDER} -- \
 	--palette ${CWD}/palettes/selenized-light.yaml \
 	-d ${CWD}/themes/common \
 	-d ${CWD}/themes/selenized \
 	-t ${CWD}/themes/selenized/light-color-theme.json.liquid \
 	>${CWD}/themes/Selenized_Light-color-theme.json
 
-cargo run -p base16cs-cli -- \
+${RENDER} -- \
 	--palette ${CWD}/palettes/selenized-dark.yaml \
 	-d ${CWD}/themes/common \
 	-d ${CWD}/themes/selenized \
 	-t ${CWD}/themes/selenized/dark-color-theme.json.liquid \
 	>${CWD}/themes/Selenized_Dark-color-theme.json
 
-cargo run -p base16cs-cli -- \
+${RENDER} -- \
 	--palette ${CWD}/palettes/solarized-light.yaml \
 	-d ${CWD}/themes/common \
 	-d ${CWD}/themes/solarized \
 	-t ${CWD}/themes/solarized/light-color-theme.json.liquid \
 	>${CWD}/themes/Solarized_Light-color-theme.json
 
-cargo run -p base16cs-cli -- \
+${RENDER} -- \
 	--palette ${CWD}/palettes/solarized-dark.yaml \
 	-d ${CWD}/themes/common \
 	-d ${CWD}/themes/solarized \
@@ -37,4 +38,3 @@ cargo run -p base16cs-cli -- \
 	>${CWD}/themes/Solarized_Dark-color-theme.json
 
 popd
-
