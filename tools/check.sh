303402597ca9e22bb93664566a7fe8ece0cc6a96
Santoso Wijaya <code@swijaya.me>
2024-05-23 03:36:27 +0000

    Script to check for themes files

---
+++ tools/check.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Compiles vscode *-color-theme.json files and compares with existing files.
+
+SOURCEDIR=$(pwd)
+THEMESDIR=${SOURCEDIR}/themes
+
+SCRIPTDIR=$(dirname "$0")
+OUTDIR=/tmp/themes
+
+mkdir -p ${OUTDIR}
+rm -rf ${OUTDIR}/*
+echo Compiling themes files in ${OUTDIR}/ ...
+${SCRIPTDIR}/regen.sh ${OUTDIR} >/dev/null
+
+echo Diffing files in ${THEMESDIR}/ ...
+for themefile in ${OUTDIR}/*.json; do
+	checkfile=${THEMESDIR}/$(basename "$themefile")
+	diff $checkfile $themefile || exit 1
+	echo "ok"
+done
+
+exit 0
